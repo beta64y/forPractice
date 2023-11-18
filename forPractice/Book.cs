cb25ba02781f09da11d395cb27b961cc378282a6
beta64 <yahyavj@code.edu.az>
2023-11-19 02:42:34 +0400

    Add project files.

---
+++ forPractice/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceOperatorTask
+{
+    internal class Book : IEntity
+    {
+        private static int IdCounter = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+
+
+        public Book(string name, string authorName, int pageCount)
+        {
+            Id = IdCounter++;
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+
+        }
+        public string ShowInfo()
+        {
+            return $"{Id} {AuthorName} {Name} {PageCount}";
+        }
+        public static bool operator >(Book book1, Book book2)
+        {
+            return book1.PageCount > book2.PageCount;
+        }
+        public static bool operator <(Book book1, Book book2)
+        {
+            return book1.PageCount < book2.PageCount;
+        }
+    }
+}
---
+++ forPractice/Library.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Exceptions;
+
+
+namespace InterfaceOperatorTask
+{
+    internal class Library : IEntity
+    {
+        static int IdCounter = 1;
+        public int Id { get;}
+        public int BookLimit { get; set; }
+        private Book[] Books;
+        public Library(int bookLimit)
+        {
+            Id = IdCounter++;
+            BookLimit = bookLimit;
+            Books = new Book[0];
+        }
+        public void AddBook(Book book)
+        {
+            if (Books.Length == BookLimit)
+            {
+                throw new CapacityLimitException("kitab qoymaga yer yoxdu abratni qayit eve");
+            }
+            if(SearchNotDeletedBookByNameInLibrary(book.Name))
+            {
+                throw new AlreadyExistsException("bu artiq var ");
+            }
+            Array.Resize(ref Books, Books.Length + 1);
+            Books[Books.Length - 1] = book;
+
+
+
+        }
+        public bool SearchNotDeletedBookByNameInLibrary(string name)
+        {
+            foreach (var book in Books)
+            {
+                if (book.Name == name & !book.IsDeleted)
+                {
+                    return true;    
+                }
+            }
+            return false;
+        }
+        public Book GetBookById(int id)
+        {
+            foreach (var book in Books)
+            {
+                if (book.Id == id & !book.IsDeleted)
+                {
+                    return book;
+                }
+            }
+            throw new NotFoundException("tapilmadi");
+        }
+
+    }
+}
